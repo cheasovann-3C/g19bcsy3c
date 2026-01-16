5cbac153d2246a8c9228cb45bb3f6fd7a38da415
Sovann <cheasovann3344@gmail.com>
2026-01-16 11:23:00 +0700

    Describe your changes

--- includes/navbar.inc.php
+++ includes/navbar.inc.php
@@ -20,11 +20,11 @@
                             Auth
                         </a>
                         <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="/g19bcsy3c/pages/login.php">Login</a></li>
+                            <li><a class="dropdown-item" href="">Login</a></li>
                             <li>
                                 <hr class="dropdown-divider">
                             </li>
-                            <li><a class="dropdown-item" href="/g19bcsy3c/pages/register.php">Register</a></li>
+                            <li><a class="dropdown-item" href="">Register</a></li>
                         </ul>
                     </li>
                     <li class="nav-item">
